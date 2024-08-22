306ccb176308cfa8eb9b1621460c79cf1b83ffad
MhmdRhayem <10121011@mu.edu.lb>
2024-08-22 10:32:40 +0300

    Add code to insert multiple articles (10000) from files in a folder

--- data_storage.py
+++ data_storage.py
@@ -1,13 +1,18 @@
 import pymongo
 import json
+import os
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["almayadeen"]
 collection = db["articles"]
 
+folder_path = "./articles"
+file_names = os.listdir(folder_path)
+print(file_names)
 
-with open('./articles/articles_2024_08.json','r',encoding='utf-8') as f:
-    data = json.load(f)
-    collection.insert_many(data)
+for file in file_names:
+    with open(f"{folder_path}/{file}", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        collection.insert_many(data)
 
 print("Data inserted successfully!")
