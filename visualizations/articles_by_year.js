43c5488f9b9154045727f3de60f769ce0d74aad8
MhmdRhayem <10121011@mu.edu.lb>
2024-08-28 15:15:57 +0300

    Add articles_by_year visualization

---
+++ visualizations/articles_by_year.js
@@ -0,0 +1,111 @@
+am5.ready(async function () {
+  var root = am5.Root.new("chartdiv");
+
+  root.setThemes([am5themes_Animated.new(root)]);
+
+  var chart = root.container.children.push(
+    am5xy.XYChart.new(root, {
+      panX: false,
+      panY: false,
+      wheelX: "none",
+      wheelY: "none",
+      paddingLeft: 0,
+    })
+  );
+
+  chart.zoomOutButton.set("forceHidden", true);
+
+  var yRenderer = am5xy.AxisRendererY.new(root, {
+    minGridDistance: 30,
+    minorGridEnabled: true,
+    inversed: true,
+  });
+
+  yRenderer.grid.template.set("location", 1);
+
+  var yAxis = chart.yAxes.push(
+    am5xy.CategoryAxis.new(root, {
+      maxDeviation: 0,
+      categoryField: "_id",
+      renderer: yRenderer,
+      tooltip: am5.Tooltip.new(root, { themeTags: ["axis"] }),
+    })
+  );
+
+  var xAxis = chart.xAxes.push(
+    am5xy.ValueAxis.new(root, {
+      maxDeviation: 0,
+      min: 0,
+      numberFormatter: am5.NumberFormatter.new(root, {
+        numberFormat: "#,###a",
+      }),
+      extraMax: 0.1,
+      renderer: am5xy.AxisRendererX.new(root, {
+        strokeOpacity: 0.1,
+        minGridDistance: 80,
+      }),
+    })
+  );
+
+  var series = chart.series.push(
+    am5xy.ColumnSeries.new(root, {
+      name: "Series 1",
+      xAxis: xAxis,
+      yAxis: yAxis,
+      valueXField: "count",
+      categoryYField: "_id",
+      tooltip: am5.Tooltip.new(root, {
+        pointerOrientation: "left",
+        labelText: "{valueX}",
+      }),
+    })
+  );
+
+  series.columns.template.setAll({
+    cornerRadiusTR: 5,
+    cornerRadiusBR: 5,
+    strokeOpacity: 0,
+  });
+
+  series.columns.template.adapters.add("fill", function (fill, target) {
+    return chart.get("colors").getIndex(series.columns.indexOf(target));
+  });
+
+  series.columns.template.adapters.add("stroke", function (stroke, target) {
+    return chart.get("colors").getIndex(series.columns.indexOf(target));
+  });
+
+  const min_year = 2012;
+  const max_year = new Date().getFullYear();
+  let year = min_year
+  data = [];
+
+  while (year <= max_year) {
+    const response = await fetch(
+      "http://127.0.0.1:5000/articles_by_year/" + year.toString()
+    );
+    articles = await response.json();
+    console.log(articles)
+    if (articles.length) {
+      data.push({ _id: year, count: articles[0].count });
+    }
+    year++;
+  }
+
+  console.log(data);
+
+  yAxis.data.setAll(data);
+  series.data.setAll(data);
+
+  chart.set(
+    "cursor",
+    am5xy.XYCursor.new(root, {
+      behavior: "none",
+      xAxis: xAxis,
+      yAxis: yAxis,
+    })
+  );
+
+  series.appear(1000);
+  chart.appear(1000, 100);
+});
