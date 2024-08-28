2712e2b0ca7a0595cd2eb3f3df77b05f5b4ae922
MhmdRhayem <10121011@mu.edu.lb>
2024-08-28 16:24:16 +0300

    Add visualization for articles with thumbnail

---
+++ visualizations/articles_with_thumbnail.js
@@ -0,0 +1,53 @@
+am5.ready(async function () {
+  var root = am5.Root.new("chartdiv");
+
+  root.setThemes([am5themes_Animated.new(root)]);
+
+  var chart = root.container.children.push(
+    am5percent.PieChart.new(root, {
+      endAngle: 270,
+    })
+  );
+
+  var series = chart.series.push(
+    am5percent.PieSeries.new(root, {
+      valueField: "count",
+      categoryField: "_id",
+      endAngle: 270,
+    })
+  );
+
+  series.states.create("hidden", {
+    endAngle: -90,
+  });
+
+  async function getArticlesWithThumbnail() {
+    const response = await fetch(
+      "http://127.0.0.1:5000/articles_with_thumbnail"
+    );
+    const data = await response.json();
+    return data;
+  }
+
+  async function getAllArticlesCount() {
+    const response = await fetch("http://127.0.0.1:5000/articles_count");
+    return await response.json();
+  }
+
+  let articles_with_thumbnail_count = (await getArticlesWithThumbnail()).length;
+  let articles_without_thumbnail_count =
+    (await getAllArticlesCount()) - articles_with_thumbnail_count;
+
+  data = [
+    {
+      _id: "Articles Without Thumbnail",
+      count: articles_without_thumbnail_count,
+    },
+    { _id: "Articles With Thumbnail", count: articles_with_thumbnail_count },
+  ];
+
+  console.log(data)
+  
+  series.data.setAll(data);
+  series.appear(1000, 100);
+});
