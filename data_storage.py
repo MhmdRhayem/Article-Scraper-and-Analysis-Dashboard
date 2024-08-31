9d3a3deadfe94bd9d541527114eac8b6d2ee3a59
MhmdRhayem <10121011@mu.edu.lb>
2024-08-31 14:17:27 +0300

    Refactor data_storage.py for improved data insertion and deletion

--- data_storage.py
+++ data_storage.py
@@ -9,8 +9,10 @@ collection = db["articles"]
 folder_path = "./articles"
 file_names = os.listdir(folder_path)
 print(file_names)
+collection.delete_many({})
 
 for file in file_names:
+    print(f"Inserting data from {file}...")
     with open(f"{folder_path}/{file}", "r", encoding="utf-8") as f:
         data = json.load(f)
         collection.insert_many(data)
