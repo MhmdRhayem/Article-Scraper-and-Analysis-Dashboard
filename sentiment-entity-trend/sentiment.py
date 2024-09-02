1217b1f96437ba5c160c5bfbdaadcd6a2afc08a4
MhmdRhayem <10121011@mu.edu.lb>
2024-09-02 17:57:57 +0300

    Add sentiment analysis functionality to create a sentiment from analyzed text

--- sentiment-entity-trend/sentiment.py
+++ sentiment-entity-trend/sentiment.py
@@ -2,3 +2,19 @@ import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 nltk.download('vader_lexicon')
+
+sia = SentimentIntensityAnalyzer()
+text = "I love this product"
+
+def classify_sentiment(text):
+    sentiment = sia.polarity_scores(text)
+    compound = sentiment['compound']
+    
+    if compound > 0.05:
+        return 'positive'
+    elif compound < -0.05:
+        return 'negative'
+    else:
+        return 'neutral'
+
+print(classify_sentiment(text))
