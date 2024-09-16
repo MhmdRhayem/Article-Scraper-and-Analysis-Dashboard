75a53dc3d6e0f4f4a7ceea1f11bb791502f15007
MhmdRhayem <10121011@mu.edu.lb>
2024-09-16 13:11:22 +0300

    Add new SCSS files for mixins, utilities, and vendor styles to make the dashboard

---
+++ startbootstrap-sb-admin-2-gh-pages/js/main_chart_script.js
@@ -0,0 +1,23 @@
+function include(file) {
+ 
+    let script = document.createElement('script');
+    script.src = file;
+    script.type = 'text/javascript';
+    script.defer = true;
+ 
+    document.getElementsByTagName('head').item(0).appendChild(script);
+ 
+}
+
+path_to_scripts = "../../visualizations/scripts/"
+const chartScripts = [
+    'articles_by_classes.js',
+    'articles_count.js',
+    'top_author.js'
+];
+
+for (let script of chartScripts){
+    script = path_to_scripts + script;
+    include(script);
+}
+
