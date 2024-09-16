c0af4d660839eb5eb8c3d5e2e4d48488b7f668d1
MhmdRhayem <10121011@mu.edu.lb>
2024-09-17 01:03:01 +0300

    Add positive_articles_count.js to display the number of positive articles

---
+++ visualizations/scripts/entities-sentiment/positive_articles_count.js
@@ -0,0 +1,28 @@
+const positiveArticlesDiv = document.getElementById("positiveArticlesCount");
+positiveArticlesDiv.style.fontSize = "24px";
+positiveArticlesDiv.style.fontWeight = "bold";
+positiveArticlesDiv.style.textAlign = "center";
+positiveArticlesDiv.style.color = "#333";
+positiveArticlesDiv.style.margin = "20px 0";
+positiveArticlesDiv.style.lineHeight = "1.5";
+positiveArticlesDiv.style.fontFamily = "'Arial', sans-serif";
+async function fetchTotalArticles() {
+  try {
+    const response = await fetch("http://127.0.0.1:5000/articles_by_sentiment_count");
+    const data = await response.json();
+    let articlesCount = 0;
+    data.forEach(ele => {
+        if (ele._id = "Positive"){
+            articlesCount = ele.count;
+        }
+    })
+
+    positiveArticlesDiv.textContent = `${articlesCount}`;
+  } catch (error) {
+    console.error("Error fetching total articles:", error);
+
+    positiveArticlesDiv.textContent = "Error fetching total articles";
+  }
+}
+
+fetchTotalArticles();
