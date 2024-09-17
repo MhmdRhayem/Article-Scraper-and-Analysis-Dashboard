2b6a8590706d59744050a0c92b73f0d8e2d6cbe2
MhmdRhayem <10121011@mu.edu.lb>
2024-09-17 14:10:14 +0300

    Refactor top_entity_by_sentiment.js to update chart dimensions and add legend

--- visualizations/scripts/entity/top_entity_by_sentiment.js
+++ visualizations/scripts/entity/top_entity_by_sentiment.js
@@ -1,7 +1,7 @@
 am5.ready(async function () {
   var chartDiv = document.getElementById("topEntityBySentiment");
   chartDiv.style.width = "100%";
-  chartDiv.style.height = "500px";
+  chartDiv.style.height = "400px";
   var root = am5.Root.new("topEntityBySentiment");
     
     
@@ -10,6 +10,7 @@ am5.ready(async function () {
     root.setThemes([
       am5themes_Animated.new(root)
     ]);
+    
     
     
     // Create chart
@@ -25,8 +26,11 @@ am5.ready(async function () {
       valueField: "count",
       categoryField: "_id"
     }));
+    series.labels.template.set("visible", false);
+    series.ticks.template.set("visible", false);
     
-    
+    series.slices.template.set("tooltipText", "{category}: {value}");
+
     // Set data
     // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
     let top_entity = ""
@@ -51,6 +55,17 @@ am5.ready(async function () {
         x: am5.percent(50),  // Centering title
         centerX: am5.percent(50)  // Centering title
       }));
+
+      var legend = chart.children.push(am5.Legend.new(root, {
+        y: am5.percent(0),
+        centerY: am5.percent(0),
+        x: am5.percent(65),
+        layout: root.verticalLayout
+      }));
+      
+      legend.data.setAll(series.dataItems);
+
+      
 
     // Play initial series animation
     // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
