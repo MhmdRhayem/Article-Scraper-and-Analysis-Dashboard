9835d9b435b86e61caa9432aa8c699135157a1a3
MhmdRhayem <10121011@mu.edu.lb>
2024-09-17 08:31:49 +0300

    Refactor load_script_buttons.js to dynamically create buttons for each script category

--- startbootstrap-sb-admin-2-gh-pages/js/load_script_buttons.js
+++ startbootstrap-sb-admin-2-gh-pages/js/load_script_buttons.js
@@ -1,47 +1,51 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const buttonsContainer = document.getElementById("button-container");
-  
-    try {
-      // Fetch the script names
-      const response = await fetch("http://127.0.0.1:5000/get_scriptfiles_names");
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-      // Parse the response as JSON
-      const chartScripts = await response.json();
-      // Loop through each script and create a button
-      chartScripts.forEach((script) => {
+  try {
+    // Fetch the script names
+    const response = await fetch("http://127.0.0.1:5000/get_scriptfiles_names");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    // Parse the response as JSON
+    const chartScripts = await response.json();
+    // Loop through each script and create a button
+    for (const category in chartScripts) { 
+      chartScripts[category].forEach((script) => {
+        // script = script.split("/")[1];
         console.log(script);
         const card = document.createElement("div");
         card.className = "mb-3 col-sm-6";
         let name_array = script.split("/");
-        let temp_arr = name_array[name_array.length - 1].split(".")[0].split("_");
+        let temp_arr = name_array[name_array.length - 1]
+          .split(".")[0]
+          .split("_");
         console.log(temp_arr);
         let name = "";
         temp_arr.forEach((word) => {
-            let formatted_word = word.charAt(0).toUpperCase() + word.slice(1) + " ";
-            if (word != "articles" && word != "by" && word != "grouped"){
-                name += formatted_word
-            }
+          let formatted_word =
+            word.charAt(0).toUpperCase() + word.slice(1) + " ";
+          if (word != "articles" && word != "by" && word != "grouped") {
+            name += formatted_word;
+          }
         });
-        
+        const buttonsContainer = document.getElementById(`${category}-buttons`);
+
         card.innerHTML = `
-                        <div class="card">
-                            <div class="card-body btn btn-outline-primary" onclick="loadChart('../../visualizations/scripts/${script}')">
-                                ${name}
+                            <div class="card">
+                                <div class="card-body btn btn-outline-primary" onclick="loadChart('../../visualizations/scripts/${script}')">
+                                    ${name}
+                                </div>
                             </div>
-                        </div>
-                    `;
+                        `;
         buttonsContainer.appendChild(card);
       });
-    } catch (error) {
-      console.error('Error fetching script files:', error);
     }
-  });
-  
-  function loadChart(scriptName) {
-    // Open blank.html and pass the script name
-    window.location.href = `blank.html?chart=${scriptName}`;
+  } catch (error) {
+    console.error("Error fetching script files:", error);
   }
-  
+});
+
+function loadChart(scriptName) {
+  // Open blank.html and pass the script name
+  window.location.href = `blank.html?chart=${scriptName}`;
+}
